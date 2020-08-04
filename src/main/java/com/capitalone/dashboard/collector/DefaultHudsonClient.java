9ee19f6265d9bb84c62ad1a83525f3670aaa00c8
Pai <prathamesh.pai@capgemini.com>
2020-08-04 18:10:38 +0530

    Update DefaultHudsonClient.java

--- src/main/java/com/capitalone/dashboard/collector/DefaultHudsonClient.java
+++ src/main/java/com/capitalone/dashboard/collector/DefaultHudsonClient.java
@@ -658,11 +658,11 @@ public class DefaultHudsonClient implements HudsonClient {
 			}
 		}
 		// Basic Auth only.
-		// if (StringUtils.isNotEmpty(userInfo)) {
-		return rest.exchange(thisuri, HttpMethod.GET, new HttpEntity<>(createHeaders(userInfo)), String.class);
-		// } else {
-		// return rest.exchange(thisuri, HttpMethod.GET, null, String.class);
-		// }
+		if (StringUtils.isNotEmpty(userInfo)) {
+			return rest.exchange(thisuri, HttpMethod.GET, new HttpEntity<>(createHeaders(userInfo)), String.class);
+		} else {
+			return rest.exchange(thisuri, HttpMethod.GET, null, String.class);
+		}
 
 	}
 
