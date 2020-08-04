281a5f8ff07e5d64320e2751a11f00f1bd9d5008
Pai <prathamesh.pai@capgemini.com>
2020-08-04 18:27:09 +0530

    Update DefaultHudsonClient.java

--- src/main/java/com/capitalone/dashboard/collector/DefaultHudsonClient.java
+++ src/main/java/com/capitalone/dashboard/collector/DefaultHudsonClient.java
@@ -91,11 +91,13 @@ public class DefaultHudsonClient implements HudsonClient {
 	@Override
 	public Map<HudsonJob, Map<jobData, Set<BaseModel>>> getInstanceJobs(String instanceUrl) {
 		LOG.debug("Enter getInstanceJobs");
+		LOG.error("Enter getInstanceJobs Prathamesh");
+
 		// Map<HudsonJob, Set<Build>> result = new LinkedHashMap<>();
 		Map<HudsonJob, Map<jobData, Set<BaseModel>>> result = new LinkedHashMap<>();
-
 		int jobsCount = getJobsCount(instanceUrl);
 		LOG.debug("Number of jobs " + jobsCount);
+		LOG.error("Number of jobs Prathamesh" + jobsCount);
 
 		int i = 0, pageSize = settings.getPageSize();
 		// Default pageSize to 1000 for backward compatibility of settings when pageSize
